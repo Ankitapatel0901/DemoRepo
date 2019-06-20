5203d8c03200c6827e7c8b4a038f322793dfb942
Ankita Patel <you@example.com>
2019-06-20 11:36:53 +0530

    first commit

---
+++ src/main/java/com/example/FirsTDemo/FirsTDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.FirsTDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirsTDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirsTDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/FirsTDemo/FirsTDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.FirsTDemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FirsTDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
